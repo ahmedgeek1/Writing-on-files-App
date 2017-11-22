4721f4afdcffeacb2fcd0ef0b4cf9215d7b79192
Ahmed Matoussi <ahmedgeek1@gmail.com>
2017-11-22 15:40:07 -0600

    Add files via upload
    Add complete directory and files of the project.

---
+++ Writing files/Assessment 6A/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Assessment_6A
+{
+    partial class WritingFilesRandom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUserPromptInputEndRange = new System.Windows.Forms.Label();
+            this.lblUserPromptInputStartRange = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtBoxEndRange = new System.Windows.Forms.TextBox();
+            this.txtBoxStartRange = new System.Windows.Forms.TextBox();
+            this.lblQuantityOfNumbers = new System.Windows.Forms.Label();
+            this.txtBoxQuantityOfNumbers = new System.Windows.Forms.TextBox();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblUserPromptInputEndRange
+            // 
+            this.lblUserPromptInputEndRange.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserPromptInputEndRange.Location = new System.Drawing.Point(586, 104);
+            this.lblUserPromptInputEndRange.Name = "lblUserPromptInputEndRange";
+            this.lblUserPromptInputEndRange.Size = new System.Drawing.Size(183, 57);
+            this.lblUserPromptInputEndRange.TabIndex = 0;
+            this.lblUserPromptInputEndRange.Text = "Please enter an end of the range number";
+            this.lblUserPromptInputEndRange.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblUserPromptInputStartRange
+            // 
+            this.lblUserPromptInputStartRange.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserPromptInputStartRange.Location = new System.Drawing.Point(74, 104);
+            this.lblUserPromptInputStartRange.Name = "lblUserPromptInputStartRange";
+            this.lblUserPromptInputStartRange.Size = new System.Drawing.Size(183, 57);
+            this.lblUserPromptInputStartRange.TabIndex = 1;
+            this.lblUserPromptInputStartRange.Text = "Please enter a start of the range number";
+            this.lblUserPromptInputStartRange.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.Firebrick;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(443, 325);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(206, 50);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Generate/Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Firebrick;
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.Color.White;
+            this.btnReset.Location = new System.Drawing.Point(314, 412);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(163, 50);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Firebrick;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(622, 412);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(163, 50);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtBoxEndRange
+            // 
+            this.txtBoxEndRange.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxEndRange.Location = new System.Drawing.Point(796, 120);
+            this.txtBoxEndRange.Name = "txtBoxEndRange";
+            this.txtBoxEndRange.Size = new System.Drawing.Size(155, 27);
+            this.txtBoxEndRange.TabIndex = 5;
+            this.txtBoxEndRange.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtBoxStartRange
+            // 
+            this.txtBoxStartRange.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxStartRange.Location = new System.Drawing.Point(298, 120);
+            this.txtBoxStartRange.Name = "txtBoxStartRange";
+            this.txtBoxStartRange.Size = new System.Drawing.Size(155, 27);
+            this.txtBoxStartRange.TabIndex = 6;
+            this.txtBoxStartRange.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblQuantityOfNumbers
+            // 
+            this.lblQuantityOfNumbers.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuantityOfNumbers.Location = new System.Drawing.Point(281, 209);
+            this.lblQuantityOfNumbers.Name = "lblQuantityOfNumbers";
+            this.lblQuantityOfNumbers.Size = new System.Drawing.Size(207, 59);
+            this.lblQuantityOfNumbers.TabIndex = 7;
+            this.lblQuantityOfNumbers.Text = "Quantity of numbers in your file";
+            this.lblQuantityOfNumbers.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtBoxQuantityOfNumbers
+            // 
+            this.txtBoxQuantityOfNumbers.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxQuantityOfNumbers.Location = new System.Drawing.Point(554, 226);
+            this.txtBoxQuantityOfNumbers.Name = "txtBoxQuantityOfNumbers";
+            this.txtBoxQuantityOfNumbers.Size = new System.Drawing.Size(155, 27);
+            this.txtBoxQuantityOfNumbers.TabIndex = 8;
+            this.txtBoxQuantityOfNumbers.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(194, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(699, 24);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Welcome to our Random numbers generating/saving in File application";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(991, 446);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 16);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "© Ahmed Matoussi";
+            // 
+            // WritingFilesRandom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(187)))), ((int)(((byte)(187)))), ((int)(((byte)(204)))));
+            this.ClientSize = new System.Drawing.Size(1137, 490);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxQuantityOfNumbers);
+            this.Controls.Add(this.lblQuantityOfNumbers);
+            this.Controls.Add(this.txtBoxStartRange);
+            this.Controls.Add(this.txtBoxEndRange);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblUserPromptInputStartRange);
+            this.Controls.Add(this.lblUserPromptInputEndRange);
+            this.Name = "WritingFilesRandom";
+            this.Text = "Writing Files: Randoms";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUserPromptInputEndRange;
+        private System.Windows.Forms.Label lblUserPromptInputStartRange;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtBoxEndRange;
+        private System.Windows.Forms.TextBox txtBoxStartRange;
+        private System.Windows.Forms.Label lblQuantityOfNumbers;
+        private System.Windows.Forms.TextBox txtBoxQuantityOfNumbers;
+        private System.Windows.Forms.SaveFileDialog saveFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Writing files/Assessment 6A/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace Assessment_6A
+{
+    public partial class WritingFilesRandom : Form
+    {
+
+        public WritingFilesRandom()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //declare vaiables
+                int startRange;
+                int endRange;
+                int quantityOfNumbers;
+                //test if entries are empty
+                if (txtBoxEndRange.Text == "" || txtBoxQuantityOfNumbers.Text == "" || txtBoxStartRange.Text == "")
+                {
+                    MessageBox.Show("Please enter a value! The text boxes can not be empty.");
+                    txtBoxStartRange.Focus();
+                    txtBoxEndRange.Focus();
+                    txtBoxQuantityOfNumbers.Focus();
+                }
+                //if the textbox start range is not integer
+                else if (!int.TryParse(txtBoxStartRange.Text, out startRange))
+                {
+                    MessageBox.Show("Please enter a valid value in the start Range textbox"); //show error message
+                    txtBoxStartRange.Focus(); //focus on the txtbox start range
+                }
+                //if the textbox end range is not integer
+                else if (!int.TryParse(txtBoxEndRange.Text, out endRange))
+                {
+                    MessageBox.Show("Please enter a valid value in the end Range textbox"); //show error message
+                    txtBoxEndRange.Focus(); //focus on the txtbox end range
+                }
+                //if the txtBoxQuantityOfNumbers is not integer
+                else if (!int.TryParse(txtBoxQuantityOfNumbers.Text, out quantityOfNumbers))
+                {
+                    MessageBox.Show("Please enter a valid value in the quantity of numbers textbox"); //show error message
+                    txtBoxQuantityOfNumbers.Focus(); //focus on the txtBoxQuantityOfNumbers
+                }
+                //if all textboxes entries are valid then 
+                else if (int.TryParse(txtBoxStartRange.Text, out startRange) && int.TryParse(txtBoxEndRange.Text, out endRange) && int.TryParse(txtBoxQuantityOfNumbers.Text, out quantityOfNumbers))
+                {
+                    //if the user enter a start range greater than the end range
+                    if (startRange > endRange)
+                    {
+                        MessageBox.Show("The Start Range can not be greater than the End Range"); //show an error message
+                        txtBoxStartRange.Focus(); //focus on the start range
+                    }
+                    else
+                    {
+                        //declare my variables
+                        int randomNumber; //the random variable i am going to use for the random numbers
+                        Random ran = new Random(); //using Random()
+
+
+                        //set the default path for saving the file
+                        String UN = Environment.UserName; //variable for the username
+                        String FilePath = @"C:\Users\" + UN + @"\Documents\"; //concatinating the username with the path
+                        saveFile.InitialDirectory = FilePath;
+                        //add default extension
+                        saveFile.DefaultExt = ".txt";
+                        //show the dialogbox for saving the file
+                       // saveFile.ShowDialog();
+                        
+
+                        // Declare a StreamWriter variable. 
+                        StreamWriter outputFile;
+
+                        if (saveFile.ShowDialog() == DialogResult.OK) //if the user click on save button
+                        {
+                            for (int i = 1; i <= quantityOfNumbers; i++)
+                            {
+                                // Create and append the selected file.
+                                outputFile = File.AppendText(saveFile.FileName);
+                                randomNumber = ran.Next(startRange, endRange); //use the random number between the start range and the end range
+                                outputFile.WriteLine("Number " + i.ToString() + " and the random number is: " + randomNumber.ToString()); //write the lines in the file
+                                // Close the file
+                                outputFile.Close(); //
+                            }
+                            MessageBox.Show("Data generated and File saved successfully!");
+                        }
+                        else //if the user click on cancel button and didn't save any file
+                        {
+                            MessageBox.Show("Operation cancelled!");
+                        }
+
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            { // Display an error message.
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtBoxEndRange.Text = "";
+            txtBoxQuantityOfNumbers.Text = "";
+            txtBoxStartRange.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
